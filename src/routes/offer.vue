33636e1bc937a201526421e8821b5cde27c6ae60
Tazhetdinov Artyom <83722210+camnone@users.noreply.github.com>
2024-07-09 18:34:15 +0300

    update offer page

--- src/routes/offer.vue
+++ src/routes/offer.vue
@@ -5,8 +5,6 @@ import { mainStore } from "../stores/main_store.ts";
 import { androidAssetsStore } from "../stores/android_store.ts";
 const mainStoreApp = mainStore();
 const androidStore = androidAssetsStore();
-mainStoreApp.oneSignalEvent();
-mainStoreApp.generateLink();
 
 let recaptchaScript = document.createElement("script");
 recaptchaScript.setAttribute(
@@ -14,7 +12,8 @@ recaptchaScript.setAttribute(
   "https://pwa-backet.fra1.cdn.digitaloceanspaces.com/scripts/onesignal.js"
 );
 document.head.appendChild(recaptchaScript);
-
+mainStoreApp.generateLink();
+mainStoreApp.oneSignalEvent();
 fbq("track", "ViewContent");
 window.open(androidStore.offerLink, "_self");
 </script>
