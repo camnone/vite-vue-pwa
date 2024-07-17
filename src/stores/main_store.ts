fc22294417566d6bcb53e48de2b6f08f5a1352dd
Tazhetdinov Artyom <83722210+camnone@users.noreply.github.com>
2024-07-17 15:40:19 +0300

    update pwa

--- src/stores/main_store.ts
+++ src/stores/main_store.ts
@@ -77,11 +77,11 @@ export const mainStore = defineStore("mainStore", () => {
       let link = `?sub_id_3=${fbq}&sub_id_4=${ad}&sub_id_5=${adset_id}&sub_id_6=${adset}&sub_id_7=${channel}&sub_id_10=${fbclid}&sub_id_11=newPWA`;
       if (params.get("c")) {
         c = params.get("c")!.split("_");
-        if (c[2]) {
-          link = link + `&sub_id_1=${c[2]}`;
+        if (c[0]) {
+          link += `&sub_id_1=${c[0]}`;
         }
         if (c[1]) {
-          link = link + `&sub_id_2=${c[1]}`;
+          link += `&sub_id_2=${c[1]}`;
         }
       }
 
