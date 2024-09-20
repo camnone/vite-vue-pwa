9ec11ec08dcd3ca9ffd439335da90ad351308b85
Tazhetdinov Artyom <83722210+camnone@users.noreply.github.com>
2024-09-20 16:28:27 +0300

    update pwa

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = "SW";
 const toCache = [];
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js");
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
